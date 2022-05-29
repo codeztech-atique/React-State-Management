f2d0b9ab8488b0a789ed9b1177f0c260f736c28d
codeztech-atique <atique1224@gmail.com>
2022-05-29 23:12:59 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import './style.css';
 
+// State Management using functional Component
+// State Management using Class Component --->
 export default function App() {
+  const [counter, setCount] = React.useState(0);
+  const incrementCount = () => {
+    setCount(counter + 1);
+    console.log(counter);
+  };
+
+  const decrementCount = () => {
+    setCount(counter - 1);
+    console.log(counter);
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <h1>{counter}</h1>
+      <button onClick={incrementCount}> Increment</button>
+      <button onClick={decrementCount}> Decrement</button>
     </div>
   );
 }
